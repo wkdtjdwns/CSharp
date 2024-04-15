5dbeacf8de08aa4b34a689aee1cafd4a277a5c0e
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-04-15 19:40:48 +0900

    Create VariablePrint.cs

---
+++ basic/03.변수/03-2. 변수 출력/VariablePrint.cs
@@ -0,0 +1,12 @@
+string name = "Sungil";
+Console.WriteLine("Hello " + name); // Hello Sungil
+
+string first_name = "Sungil ";
+string last_name = "Infomation";
+string full_name = first_name + last_name ;
+
+Console.WriteLine(full_name); // Sungil Infomation
+
+int x = 5;
+int y = 6;
+Console.WriteLine(x + y); // 5 + 6의 값인 11이 출력 됨
