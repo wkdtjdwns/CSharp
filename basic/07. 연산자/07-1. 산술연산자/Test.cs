6f755ac0ae8464560374ecac81fccb21d16bffed
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-04-16 20:25:00 +0900

    Create Test.cs

---
+++ basic/07. 연산자/07-1. 산술연산자/Test.cs
@@ -0,0 +1,8 @@
+int a = 10;
+int b = 2;
+
+Console.WriteLine("a + b : " + (a + b)); // a + b : 12
+Console.WriteLine("a - b : " + (a - b)); // a - b : 8
+Console.WriteLine("a * b : " + (a * b)); // a * b : 20
+Console.WriteLine("a / b : " + (a / b)); // a / b : 5
+Console.WriteLine("a % b : " + (a % b)); // a % b : 0
