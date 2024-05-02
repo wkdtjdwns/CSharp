f8f10f211b847fdb2838f13f9bfb82bd63a1f4ff
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-05-02 19:34:26 +0900

    Create switch.cs

---
+++ basic/11. 조건문/11-4. switch문/switch.cs
@@ -0,0 +1,34 @@
+int day = 3;
+
+switch (day) 
+{
+  case 1:
+    Console.WriteLine("월요일");
+    break;
+    
+  case 2:
+    Console.WriteLine("화요일");
+    break;
+    
+  case 3:
+    Console.WriteLine("수요일");
+    break;
+    
+  case 4:
+    Console.WriteLine("목요일");
+    break;
+    
+  case 5:
+    Console.WriteLine("금요일");
+    break;
+    
+  case 6:
+    Console.WriteLine("토요일");
+    break;
+    
+  case 7:
+    Console.WriteLine("일요일");
+    break;
+}
+
+// Wednesday
