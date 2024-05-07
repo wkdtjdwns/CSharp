33e4fe8dbe13ed927c809685593779bed58dcb00
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-05-08 08:39:51 +0900

    Update 킹, 퀸, 룩, 비숍, 나이트, 폰.cs

--- beakjoon/Bronze/Bronze V/3003. 킹, 퀸, 룩, 비숍, 나이트, 폰/킹, 퀸, 룩, 비숍, 나이트, 폰.cs
+++ beakjoon/Bronze/Bronze V/3003. 킹, 퀸, 룩, 비숍, 나이트, 폰/킹, 퀸, 룩, 비숍, 나이트, 폰.cs
@@ -1,13 +1,8 @@
 int[] chess = { 1, 1, 2, 2, 2, 8 };
 string[] input = Console.ReadLine().Split();
-int[] result = new int[6];
 
 for (int i =  0; i < input.Length; i++)
 {
-    result[i] = chess[i] - int.Parse(input[i]);
-}
-
-foreach (int num in result)
-{
-    Console.Write(num + " ");
+    int result = chess[i] - int.Parse(input[i]);
+    Console.Write(result + " ");
 }
