d04decb813af47c165eac6cf8f4ce4ef019ca7e8
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-05-17 07:39:26 +0900

    Create Plane.cs

---
+++ beakjoon/Bronze/Bronze V/8370. Plane/Plane.cs
@@ -0,0 +1,7 @@
+string[] input = Console.ReadLine().Split(' ');
+int n1 = int.Parse(input[0]);
+int k1 = int.Parse(input[1]);
+int n2 = int.Parse(input[2]);
+int k2 = int.Parse(input[3]);
+
+Console.WriteLine((n1 * k1) + (n2 * k2));
