e8bb81c19cdba1ca5585936cd974d4dc6f472315
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-05-28 19:05:35 +0900

    Create 큰 수 A+B.cs

---
+++ beakjoon/Bronze/Bronze V/10757. 큰 수 A+B/큰 수 A+B.cs
@@ -0,0 +1,7 @@
+using System.Numerics;
+
+string[] input = Console.ReadLine().Split(" ");
+
+BigInteger A = BigInteger.Parse(input[0]);
+BigInteger B = BigInteger.Parse(input[1]);
+Console.WriteLine(A + B);
