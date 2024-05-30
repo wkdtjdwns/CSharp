dd76483a425e82e08832e60565c70c690d711edb
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-05-31 08:24:07 +0900

    Create A+B - 8.cs

---
+++ beakjoon/Bronze/Bronze V/11022. A+B - 8/A+B - 8.cs
@@ -0,0 +1,9 @@
+int n = int.Parse(Console.ReadLine());
+for (int i = 0; i < n; i++)
+{
+    string[] input = Console.ReadLine().Split(" ");
+    int a = int.Parse(input[0]);
+    int b = int.Parse(input[1]);
+
+    Console.WriteLine($"Case #{i + 1}: {a} + {b} = {a + b}");
+}
