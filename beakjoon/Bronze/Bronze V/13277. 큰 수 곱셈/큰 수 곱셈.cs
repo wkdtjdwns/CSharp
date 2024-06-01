6b519b87914d75d50f691271eb7f7414a89b044e
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-06-02 08:55:38 +0900

    Create 큰 수 곱셈.cs

---
+++ beakjoon/Bronze/Bronze V/13277. 큰 수 곱셈/큰 수 곱셈.cs
@@ -0,0 +1,7 @@
+using System.Numerics;
+
+string[] input = Console.ReadLine().Split(" ");
+BigInteger a = BigInteger.Parse(input[0]);
+BigInteger b = BigInteger.Parse(input[1]);
+
+Console.WriteLine(a * b);
