c6db75fb31552e5fb04943ec746dca1ae8f1422a
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-06-01 21:13:09 +0900

    Create 꼬마 정민.cs

---
+++ beakjoon/Bronze/Bronze V/11382. 꼬마 정민/꼬마 정민.cs
@@ -0,0 +1,8 @@
+using System.Numerics;
+
+string[] input = Console.ReadLine().Split(" ");
+BigInteger a = BigInteger.Parse(input[0]);
+BigInteger b = BigInteger.Parse(input[1]);
+BigInteger c = BigInteger.Parse(input[2]);
+
+Console.WriteLine(a + b + c);
