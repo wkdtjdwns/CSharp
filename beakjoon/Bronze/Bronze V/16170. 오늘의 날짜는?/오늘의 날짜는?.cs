26a4a5892699ad37c45f1101bc61f8d6b33e8c16
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-06-04 10:20:19 +0900

    Create 오늘의 날짜는?.cs

---
+++ beakjoon/Bronze/Bronze V/16170. 오늘의 날짜는?/오늘의 날짜는?.cs
@@ -0,0 +1,9 @@
+DateTime currentUtc0Time = DateTime.UtcNow;
+
+string year = currentUtc0Time.ToString("yyyy");
+string month = currentUtc0Time.ToString("MM");
+string day = currentUtc0Time.ToString("dd");
+
+Console.WriteLine(year);
+Console.WriteLine(month);
+Console.WriteLine(day);
