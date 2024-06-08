1028b0e96e37bbcf8555a8232d9183b981f97d8c
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-06-08 10:45:52 +0900

    Create 코드마스터 2023.cs

---
+++ beakjoon/Bronze/Bronze V/28235. 코드마스터 2023/코드마스터 2023.cs
@@ -0,0 +1,8 @@
+string input = Console.ReadLine();
+switch (input)
+{
+    case "SONGDO": Console.WriteLine("HIGHSCHOOL"); break;
+    case "CODE": Console.WriteLine("MASTER"); break;
+    case "2023": Console.WriteLine("0611"); break;
+    default: Console.WriteLine("CONTEST"); break;
+}
