da0e34c2744a62924eb3173f0fb387ecc4582b5f
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-06-08 10:30:43 +0900

    Create 특별한 학교 이름.cs

---
+++ beakjoon/Bronze/Bronze V/27889. 특별한 학교 이름/특별한 학교 이름.cs
@@ -0,0 +1,8 @@
+string str = Console.ReadLine();
+switch (str)
+{
+    case "NLCS": Console.WriteLine("North London Collegiate School"); break;
+    case "BHA": Console.WriteLine("Branksome Hall Asia"); break;
+    case "KIS": Console.WriteLine("Korea International School"); break;
+    default: Console.WriteLine("St. Johnsbury Academy"); break;
+}
