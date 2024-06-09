3ef3a3694e3d73186abe81ce954da7df14664ac1
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-06-09 12:43:00 +0900

    Create 정보보호학부 동아리 소개.cs

---
+++ beakjoon/Bronze/Bronze V/28691. 정보보호학부 동아리 소개/정보보호학부 동아리 소개.cs
@@ -0,0 +1,9 @@
+string club = Console.ReadLine();
+switch (club)
+{
+    case "M": Console.WriteLine("MatKor"); break;
+    case "W": Console.WriteLine("WiCys"); break;
+    case "C": Console.WriteLine("CyKor"); break;
+    case "A": Console.WriteLine("AlKor"); break;
+    default: Console.WriteLine("$clear"); break;
+}
