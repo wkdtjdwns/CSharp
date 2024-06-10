043a7b3f206265f2d4ea00f23a9769c84c9fb27a
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-06-10 10:04:38 +0900

    Create 이 별은 무슨 색일까.cs

---
+++ beakjoon/Bronze/Bronze V/30676. 이 별은 무슨 색일까/이 별은 무슨 색일까.cs
@@ -0,0 +1,35 @@
+int lambda = int.Parse(Console.ReadLine());
+if (lambda >= 620)
+{
+    Console.WriteLine("Red");
+}
+
+else if (lambda >= 590)
+{
+    Console.WriteLine("Orange");
+}
+
+else if (lambda >= 570)
+{
+    Console.WriteLine("Yellow");
+}
+
+else if (lambda >= 495)
+{
+    Console.WriteLine("Green");
+}
+
+else if (lambda >= 450)
+{
+    Console.WriteLine("Blue");
+}
+
+else if (lambda >= 425)
+{
+    Console.WriteLine("Indigo");
+}
+
+else
+{
+    Console.WriteLine("Violet");
+}
