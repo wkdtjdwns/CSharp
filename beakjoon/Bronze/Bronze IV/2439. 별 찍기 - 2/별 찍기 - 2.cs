ea616a2d74219047bed642ccafeafaba8fe3800a
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-06-11 09:22:30 +0900

    Create 별 찍기 - 2.cs

---
+++ beakjoon/Bronze/Bronze IV/2439. 별 찍기 - 2/별 찍기 - 2.cs
@@ -0,0 +1,14 @@
+int n = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= n; i++)
+{
+    for (int j = 0; j < (n - i); j++) {
+        Console.Write(" ");
+    }
+
+    for (int k = 0; k < i; k++) {
+        Console.Write("*");
+    }
+
+    Console.WriteLine();
+}
