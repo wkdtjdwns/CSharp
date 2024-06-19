43dfc5382c27a8563e7968b469bb339251e17128
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-06-19 10:03:51 +0900

    [2024-06-19]

---
+++ beakjoon/Bronze/Bronze IV/15873. 공백 없는 A+B/공백 없는 A+B.cs
@@ -0,0 +1,16 @@
+int n = int.Parse(Console.ReadLine());
+
+if (n < 100)
+{
+    Console.WriteLine(n / 10 + n % 10);
+}
+    
+else if (n % 10 == 0)
+{
+    Console.WriteLine(n / 100 + 10);
+}
+
+else
+{
+    Console.WriteLine(n % 100 + 10);
+}
