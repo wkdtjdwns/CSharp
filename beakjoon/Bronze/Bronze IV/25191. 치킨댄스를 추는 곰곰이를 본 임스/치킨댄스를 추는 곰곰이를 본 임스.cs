a1a206c6a8ae4164878aaf2896963c5cde662e22
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-06-22 15:35:00 +0900

    [2024-06-22]

---
+++ beakjoon/Bronze/Bronze IV/25191. 치킨댄스를 추는 곰곰이를 본 임스/치킨댄스를 추는 곰곰이를 본 임스.cs
@@ -0,0 +1,6 @@
+int n = int.Parse(Console.ReadLine());
+string[] ab = Console.ReadLine().Split(" ");
+int a = int.Parse(ab[0]);
+int b = int.Parse(ab[1]);
+
+Console.WriteLine(Math.Min(n, (a / 2 + b)));
