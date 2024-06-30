dadf84c4f20d8bb09827d135c73744afff47f51c
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-06-30 12:56:14 +0900

    [2024-06-30]

---
+++ beakjoon/Bronze/Bronze IV/31403. A + B - C/A + B - C.cs
@@ -0,0 +1,8 @@
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int c = int.Parse(Console.ReadLine());
+
+int num = a + b - c;
+int str = int.Parse(a.ToString() + b.ToString()) - c;
+Console.WriteLine(num);
+Console.WriteLine(str);
