e25214e7920a3476f70f1c60d33b84d4529f665e
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-07-13 13:33:19 +0900

    [2024-07-12]

---
+++ beakjoon/Bronze/Bronze III/2997. 네 번째 수/네 번째 수.cs
@@ -0,0 +1,11 @@
+string[] nums = Console.ReadLine().Split(" ");
+int[] abc = new int[3];
+for (int i = 0; i < 3; i++) { abc[i] = int.Parse(nums[i]); }
+Array.Sort(abc);
+
+int n = abc[1] - abc[0];
+int n2 = abc[2] - abc[1];
+
+if (n == n2) { Console.WriteLine(abc[2] + n); }
+else if (n < n2) { Console.WriteLine(abc[1] + n); }
+else { Console.WriteLine(abc[0] + n2); }
