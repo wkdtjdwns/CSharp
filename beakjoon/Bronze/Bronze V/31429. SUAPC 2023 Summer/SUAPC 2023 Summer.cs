f365cffc01e65673559f54af9bb87fa3d77d683e
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-07-15 15:58:14 +0900

    [2024-07-15]

---
+++ beakjoon/Bronze/Bronze V/31429. SUAPC 2023 Summer/SUAPC 2023 Summer.cs
@@ -0,0 +1,12 @@
+int n = int.Parse(Console.ReadLine());
+if (n == 1) { Console.WriteLine(12 + " " + 1600); }
+else if (n == 2) { Console.WriteLine(11 + " " + 894); }
+else if (n == 3) { Console.WriteLine(11 + " " + 1327); }
+else if (n == 4) { Console.WriteLine(10 + " " + 1311); }
+else if (n == 5) { Console.WriteLine(9 + " " + 1004); }
+else if (n == 6) { Console.WriteLine(9 + " " + 1178); }
+else if (n == 7) { Console.WriteLine(9 + " " + 1357); }
+else if (n == 8) { Console.WriteLine(8 + " " + 837); }
+else if (n == 9) { Console.WriteLine(7 + " " + 1055); }
+else if (n == 10) { Console.WriteLine(6 + " " + 556); }
+else if (n == 11) { Console.WriteLine(6 + " " + 773); }
