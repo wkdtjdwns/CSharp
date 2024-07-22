31c6b3e5cef12315e3519454d7d0b8caee854834
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-07-22 16:56:29 +0900

    Delete beakjoon/Bronze/Bronze IV/9295. 주사위/주사위.cs

--- beakjoon/Bronze/Bronze IV/9295. 주사위/주사위.cs
+++
@@ -1,9 +0,0 @@
-int n = int.Parse(Console.ReadLine());
-for (int i = 1; i <= n; i++)
-{
-    string[] ab = Console.ReadLine().Split(" ");
-    int a = int.Parse(ab[0]);
-    int b = int.Parse(ab[1]);
-
-    Console.WriteLine($"Case {i}: {a + b}");
-}
