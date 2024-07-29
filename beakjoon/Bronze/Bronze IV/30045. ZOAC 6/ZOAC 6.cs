c90dc63aaaf61a00cd0210302cd9518fe98b7b16
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-07-29 16:13:25 +0900

    [2024-07-28]

---
+++ beakjoon/Bronze/Bronze IV/30045. ZOAC 6/ZOAC 6.cs
@@ -0,0 +1,9 @@
+int n = int.Parse(Console.ReadLine());
+int cnt = 0;
+for (int i = 0; i < n; i++)
+{
+    string str = Console.ReadLine();
+    if (str.Contains("01") || str.Contains("OI")) { cnt++; }
+}
+
+Console.WriteLine(cnt);
