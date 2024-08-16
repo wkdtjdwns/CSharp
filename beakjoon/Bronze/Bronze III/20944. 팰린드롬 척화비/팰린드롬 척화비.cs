83951764ee941654ba0dc712ebd4dec95df64433
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-08-16 13:28:09 +0900

    [2024-08-16]

---
+++ beakjoon/Bronze/Bronze III/20944. 팰린드롬 척화비/팰린드롬 척화비.cs
@@ -0,0 +1,10 @@
+using System.Text;
+
+StringBuilder sb = new StringBuilder();
+int n = int.Parse(Console.ReadLine());
+for (int i = 0; i < n; i++)
+{
+    sb.Append('s');
+}
+
+Console.WriteLine(sb);
