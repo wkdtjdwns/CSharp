79e3ccd43e3023c4fe8f3c5c29721bc1fde08be9
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-08-18 16:54:22 +0900

    [2024-08-18]

---
+++ beakjoon/Bronze/Bronze III/23802. 골뱅이 찍기 - 뒤집힌 ㄱ/골뱅이 찍기 - 뒤집힌 ㄱ.cs
@@ -0,0 +1,20 @@
+int n = int.Parse(Console.ReadLine());
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < n * 5; j++)
+    {
+        Console.Write("@");
+    }
+
+    Console.WriteLine();
+}
+
+for (int i = 0; i < n * 4; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        Console.Write("@");
+    }
+
+    Console.WriteLine();
+}
