5323bbf5b409772b4d0654c52446593ca8dd9438
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-08-20 12:25:35 +0900

    [2024-08-20]

---
+++ beakjoon/Bronze/Bronze III/23812. 골뱅이 찍기 - 돌아간 ㅍ/골뱅이 찍기 - 돌아간 ㅍ.cs
@@ -0,0 +1,85 @@
+using System.Text;
+
+StringBuilder sb = new StringBuilder();
+int n = int.Parse(Console.ReadLine());
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        sb.Append("@");
+    }
+
+    for (int j = 0; j < n * 3; j++)
+    {
+        sb.Append(" ");
+    }
+
+    for (int j = 0; j < n; j++)
+    {
+        sb.Append("@");
+    }
+
+    sb.AppendLine();
+}
+
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < n * 5; j++)
+    {
+        sb.Append("@");
+    }
+
+    sb.AppendLine();
+}
+
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        sb.Append("@");
+    }
+
+    for (int j = 0; j < n * 3; j++)
+    {
+        sb.Append(" ");
+    }
+
+    for (int j = 0; j < n; j++)
+    {
+        sb.Append("@");
+    }
+
+    sb.AppendLine();
+}
+
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < n * 5; j++)
+    {
+        sb.Append("@");
+    }
+
+    sb.AppendLine();
+}
+
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        sb.Append("@");
+    }
+
+    for (int j = 0; j < n * 3; j++)
+    {
+        sb.Append(" ");
+    }
+
+    for (int j = 0; j < n; j++)
+    {
+        sb.Append("@");
+    }
+
+    sb.AppendLine();
+}
+
+Console.WriteLine(sb);
