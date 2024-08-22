67a69e51be07923f44a8c699f443008f0b24c8c8
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-08-22 09:46:29 +0900

    [2024-08-22]

---
+++ beakjoon/Bronze/Bronze III/24724. 현대모비스와 함께하는 부품 관리/현대모비스와 함께하는 부품 관리.cs
@@ -0,0 +1,15 @@
+int t = int.Parse(Console.ReadLine());
+for (int i = 1; i <= t; i++)
+{
+    int n = int.Parse(Console.ReadLine());
+    string uv = Console.ReadLine();
+
+    Console.WriteLine("Material Management " + i);
+
+    for (int j = 0; j < n; j++)
+    {
+        uv = Console.ReadLine();
+    }
+
+    Console.WriteLine("Classification ---- End!");
+}
