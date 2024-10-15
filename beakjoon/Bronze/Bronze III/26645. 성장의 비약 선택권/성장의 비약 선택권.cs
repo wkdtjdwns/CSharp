a2c6f352b62bd751ea6c18833faf4a7fd23a771a
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-10-15 20:49:35 +0900

    [2024-10-15]

---
+++ beakjoon/Bronze/Bronze III/26645. 성장의 비약 선택권/성장의 비약 선택권.cs
@@ -0,0 +1,20 @@
+int level = int.Parse(Console.ReadLine());
+if (level < 206)
+{
+    Console.WriteLine(1);
+}
+
+else if (level < 218)
+{
+    Console.WriteLine(2);
+}
+
+else if (level < 229)
+{
+    Console.WriteLine(3);
+}
+
+else
+{
+    Console.WriteLine(4);
+}
