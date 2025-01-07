df4d909049681eec10655032868ab65c1d59b734
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2025-01-07 19:11:20 +0900

    [2025-01-07]

---
+++ beakjoon/Bronze/Bronze III/32399. 햄버거 정렬/햄버거 정렬.cs
@@ -0,0 +1,15 @@
+string str = Console.ReadLine();
+if (str.Equals(")1("))
+{
+    Console.WriteLine(2);
+}
+
+else if (str.Equals("(1)"))
+{
+    Console.WriteLine(0);
+}
+
+else
+{
+    Console.WriteLine("1");
+}
